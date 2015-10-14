397cd333ba2955e964664310caf6d5b2f4a6433f
Shashank <shashankv@live.in>
2015-10-15 00:56:44 +0530

    v0.3

--- src/com/shashank/MelodyChat/Login.java
+++ src/com/shashank/MelodyChat/Login.java
@@ -6,10 +6,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JToggleButton;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class Login extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtName;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -31,12 +40,60 @@ public class Login extends JFrame {
 	 * Create the frame.
 	 */
 	public Login() {
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		setResizable(false);
+		setTitle(" Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 301, 381);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblName = new JLabel("Name");
+		lblName.setBounds(129, 24, 56, 16);
+		contentPane.add(lblName);
+		
+		txtName = new JTextField();
+		txtName.setBounds(88, 53, 141, 22);
+		contentPane.add(txtName);
+		txtName.setColumns(10);
+		
+		JLabel lblIpAddress = new JLabel("IP Address");
+		lblIpAddress.setBounds(129, 100, 86, 16);
+		contentPane.add(lblIpAddress);
+		
+		textField = new JTextField();
+		textField.setBounds(88, 129, 141, 22);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblPort = new JLabel("Port");
+		lblPort.setBounds(142, 183, 56, 16);
+		contentPane.add(lblPort);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(88, 201, 141, 22);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(107, 287, 97, 25);
+		contentPane.add(btnLogin);
 	}
-
 }
